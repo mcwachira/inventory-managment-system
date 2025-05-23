adf9c73442d546f8ad94d5bc7c9ceff57c03fc80
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-05-24 00:04:44 +0300

    Created the ADD Items Page

---
+++ src/app/items/add/page.tsx
@@ -0,0 +1,11 @@
+import ItemFormPage from "@/components/items/ItemFormPage";
+
+const AddItemPage = async () => {
+  return (
+    <>
+      <ItemFormPage />;
+    </>
+  );
+};
+
+export default AddItemPage;
